a745616815f7c6c16991ba7d5afaffb3e00f112e
shubh1400 <113843266+shubh1400@users.noreply.github.com>
2024-07-12 15:44:31 +0000

    conversion of data type

---
+++ basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let number = 33
+
+console.log(typeof number);
+console.log(typeof(number))
+
+let stringOfNumber="33"
+
+let conversionString=Number(stringOfNumber)
+console.log(typeof stringOfNumber);
+
+
+
+// '33'=33
+//"33abc"=Nan
+//true=1, false=0
+
+let values=1
+
+let conversionBoolean=Boolean(values)
+console.log(conversionBoolean);
+
+//1 = true ,0=false
+//" "=false
+//"abc"=true
+
+let sumNumber = 33
+let strinNumber=String(sumNumber)
+console.log(strinNumber);
+console.log(typeof strinNumber);
+
