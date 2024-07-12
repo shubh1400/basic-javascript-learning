2f94e5fbcfebf391c0038e01edc7695c105e5bd2
shubh1400 <113843266+shubh1400@users.noreply.github.com>
2024-07-12 15:16:29 +0000

    variable

---
+++ basics/01_variables.js
@@ -0,0 +1,17 @@
+const Id=1234
+let Email ="xyz@gmail.com"
+var Password ="12345"
+city="pune"
+let State;
+
+Email="abc@gmail.com"
+Password='321'
+city="kolhapur"
+
+console.log(Id);
+
+
+/*
+Prefer not to ues var because of issue in block scope and funtional scope
+*/
+console.table([Email,Password,city,State])
