fd07272354ecf37bff217381d34161369f4b88a5
shubh1400 <113843266+shubh1400@users.noreply.github.com>
2024-07-12 15:16:59 +0000

    data type

---
+++ basics/02_dataType.js
@@ -0,0 +1,18 @@
+let number = [1,2,3,4] //number
+let name ="shubham" //string 
+let ss =true //boolean true/ false
+
+
+console.log(typeof null) // object
+console.log(typeof undefined) //undefined
+
+/*
+Number = 1,2,3
+bigint 
+string = " "
+boolean = true / false
+null = standalone value
+undefind =
+symbole = unigue
+*/
+
